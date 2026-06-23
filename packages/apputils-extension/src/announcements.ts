c16503a4fb7582bbda15e75b6ca7054ea0d3a57a
wnlee <wnlee@tilon.com>
2026-06-23 20:34:58 +0900

    fix : 중간점검2

--- packages/apputils-extension/src/announcements.ts
+++ packages/apputils-extension/src/announcements.ts
@@ -58,7 +58,7 @@ export const announcements: JupyterFrontEndPlugin<void> = {
   id: '@jupyterlab/apputils-extension:announcements',
   description:
     'Add the announcement feature. It will fetch news on the internet and check for application updates.',
-  autoStart: false,
+  autoStart: true,
   optional: [IConfigSectionManager, ISettingRegistry, ITranslator],
   activate: (
     app: JupyterFrontEnd,
@@ -105,7 +105,7 @@ export const announcements: JupyterFrontEndPlugin<void> = {
       if (mustFetchNews === 'none') {
         const notificationId = Notification.emit(
           trans.__(
-            'Would you like to get notified about official Jupyter news22212?'
+            'Would you like to get notified about official Jupyter news?'
           ),
           'default',
           {
